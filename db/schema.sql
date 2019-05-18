786d1eae638b9c78c48b57f95d4b2505c8d5ed4a
Quyen Huynh <quyenh@hotmail.com>
2019-05-18 11:30:30 -0400

    jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE table burgers (
-id INTEGER auto_increment,
+id INTEGER NOT NULL auto_increment,
 burger_name VARCHAR(50) NOT NULL,
 devoured boolean NOT NULL default 0,
+date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id)
 );
