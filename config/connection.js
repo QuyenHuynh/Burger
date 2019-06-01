e4d77c4e64f0579e5235abe3b95ccbfceab7325b
Quyen Huynh <quyenh@hotmail.com>
2019-06-01 14:00:06 -0400

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,10 @@ const mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
+    //Database is JawsDB on Heroku
    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+    //Database is local
     connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
